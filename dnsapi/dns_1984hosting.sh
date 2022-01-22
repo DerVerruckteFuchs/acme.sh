cf1d6635f322825dc2c208ffebd477d0b43eae60
DerVerruckteFuchs <derverrucktefuchs@protonmail.ch>
2022-01-22 15:11:18 -0500

    change debug output for

--- dnsapi/dns_1984hosting.sh
+++ dnsapi/dns_1984hosting.sh
@@ -145,8 +145,8 @@ _1984hosting_login() {
   if [ -n "$One984HOSTING_TOTP_Secret" ]; then
     _debug "Generating OTP from TOTP secret"
     otpkey="$(oathtool --base32 --totp "${One984HOSTING_TOTP_Secret}" 2>/dev/null)"
-    otpkey=$(printf '%s' "$otpkey" | _url_encode)
-    _debug2 otp "$otpkey"
+    #otpkey=$(printf '%s' "$otpkey" | _url_encode)
+    _debug otp "$otpkey"
   fi
 
   response="$(_post "username=$username&password=$password&otpkey=$otpkey" $url)"
